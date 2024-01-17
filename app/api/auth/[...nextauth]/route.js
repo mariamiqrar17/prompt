39d331498571c12c91cf5aed339688a65fd5d9af
mariamiqrar17 <mariam_iqrar_intern@netixsol.com>
2024-01-17 08:00:15 +0500

    change

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,5 @@
 import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
-import GitHubProvider from 'next-auth/providers/github'; // Import GitHubProvider
 
 import User from '@models/user';
 import { connectToDB } from '@utils/database';
@@ -10,11 +9,7 @@ const handler = NextAuth({
     GoogleProvider({
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    }),
-    GitHubProvider({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_CLIENT_SECRET,
-    }),
+    })
   ],
   callbacks: {
     async session({ session }) {
@@ -40,13 +35,13 @@ const handler = NextAuth({
           });
         }
 
-        return true;
+        return true
       } catch (error) {
         console.log("Error checking if user exists: ", error.message);
-        return false;
+        return false
       }
     },
-  },
-});
+  }
+})
 
-export { handler as GET, handler as POST };
+export { handler as GET, handler as POST }
